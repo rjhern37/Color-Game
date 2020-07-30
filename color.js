6380178deb8ae39a31db5087158353cf132fe106
rjhern37 <rjhern37@neiu.edu>
2020-07-30 14:28:17 -0500

    able to click squares

--- color.js
+++ color.js
@@ -1,4 +1,8 @@
 // alert("stay winning");
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
+
+
 
 var colors = [
     "rgb(255, 0, 0)",
@@ -10,7 +14,38 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+
+colorDisplay.textContent = pickedColor;
+
+
+
 
 for(var i = 0; i < squares.length; i++){
+    //Add colors to squares
     squares[i].style.backgroundColor = colors[i]
+
+    //Add click events to square
+    squares[i].addEventListener("click", function(){
+        //Grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        //compare color to picked color
+        if(clickedColor === pickedColor){
+           messageDisplay.textContent = "Correct";
+           changeColors(clickedColor); 
+        } else {
+            this.style.backgroundColor = "cornflowerblue";
+            messageDisplay.textContent = "Try Again";
+        }
+    });
 }
+
+function changeColors (color){
+    //loop through all squares
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+    //change each color to match given color
+}
+
+
