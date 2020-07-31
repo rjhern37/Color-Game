f1fc73696693920765196d0e9eef4f1bdfa86a66
rjhern37 <rjhern37@neiu.edu>
2020-07-30 22:45:53 -0500

    getting creative

--- color.js
+++ color.js
@@ -1,34 +1,65 @@
-// alert("stay winning");
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector("h1");
 var resetBtn = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
-var colors = generateRandomColors(6);
+
+easyBtn.addEventListener("click", function(){
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    h1.style.backgroundColor = "cornflowerblue";
+    for (var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        } else{
+            squares[i].style.display = "none"; 
+        } 
+    }
+});
+
+hardBtn.addEventListener("click", function(){
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    h1.style.backgroundColor = "cornflowerblue";
+    for (var i = 0; i < squares.length; i++){
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = "block";
+        
+    }
+});
 
 resetBtn.addEventListener("click", function(){
     //generate new colors
-    colors = generateRandomColors(6);
-
+    colors = generateRandomColors(numSquares);
     //pick new random color from arr
     pickedColor = pickColor();
     //change color display to match picked color
     colorDisplay.textContent = pickedColor;
+    //reset h1 background color
+    h1.style.backgroundColor = "cornflowerblue";
     //change colors of squares
     for(var i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = colors[i];
-        h1.style.backgroundColor = "cornflowerblue";
     }
+});
 
-})
-
-
-
-
-var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
 
 colorDisplay.textContent = pickedColor;
+
 
 for(var i = 0; i < squares.length; i++){
     //Add colors to squares
@@ -38,7 +69,7 @@ for(var i = 0; i < squares.length; i++){
     squares[i].addEventListener("click", function(){
         //Grab color of clicked square
         var clickedColor = this.style.backgroundColor;
-        console.log(clickedColor, pickedColor);
+        // console.log(clickedColor, pickedColor);
         //compare color to picked color
         if(clickedColor === pickedColor){
            messageDisplay.textContent = "Correct";
@@ -49,11 +80,12 @@ for(var i = 0; i < squares.length; i++){
             messageDisplay.textContent = "Try Again";
         }
     });
-}
+};
 
 function changeColors (color){
     //loop through all squares
     for(var i = 0; i < squares.length; i++){
+        //change each color to match gievn color
         squares[i].style.backgroundColor = color;
     }
 }
